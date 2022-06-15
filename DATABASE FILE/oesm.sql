1de09bc4e89ccc58534d6fbae6e367a9ad5bb2b8
pravinpandey721 <pravinpandey721844@gmail.com>
2022-06-16 01:13:00 +0530

    college_project

---
+++ DATABASE FILE/oesm.sql
@@ -0,0 +1,274 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 15, 2022 at 04:54 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `oesm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `email` varchar(50) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `college` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`email`, `password`, `gender`, `college`) VALUES
+('pravinpandey721844@gmail.com', 'pravin', 'M', 'sices'),
+('pravin@gmail.com', 'pravin', 'M', 'sices');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answer`
+--
+
+CREATE TABLE `answer` (
+  `qid` text NOT NULL,
+  `ansid` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `answer`
+--
+
+INSERT INTO `answer` (`qid`, `ansid`) VALUES
+('55892169bf6a7', '55892169d2efc'),
+('5589216a3646e', '5589216a48722'),
+('558922117fcef', '5589221195248'),
+('55892211e44d5', '55892211f1fa7'),
+('558922894c453', '558922895ea0a'),
+('558922899ccaa', '55892289aa7cf'),
+('558923538f48d', '558923539a46c'),
+('55892353f05c4', '55892354051be'),
+('607336aa8c987', '607336aa961b5'),
+('607336aacedd1', '607336aadc68e'),
+('607336ab244aa', '607336ab31664');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` text NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `subject` varchar(500) NOT NULL,
+  `feedback` varchar(500) NOT NULL,
+  `date` date NOT NULL,
+  `time` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history`
+--
+
+CREATE TABLE `history` (
+  `email` varchar(50) NOT NULL,
+  `eid` text NOT NULL,
+  `score` int(11) NOT NULL,
+  `level` int(11) NOT NULL,
+  `sahi` int(11) NOT NULL,
+  `wrong` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `options`
+--
+
+CREATE TABLE `options` (
+  `qid` varchar(50) NOT NULL,
+  `option` varchar(5000) NOT NULL,
+  `optionid` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `options`
+--
+
+INSERT INTO `options` (`qid`, `option`, `optionid`) VALUES
+('55892169bf6a7', 'usermod', '55892169d2efc'),
+('55892169bf6a7', 'useradd', '55892169d2f05'),
+('55892169bf6a7', 'useralter', '55892169d2f09'),
+('55892169bf6a7', 'groupmod', '55892169d2f0c'),
+('5589216a3646e', '751', '5589216a48713'),
+('5589216a3646e', '752', '5589216a4871a'),
+('5589216a3646e', '754', '5589216a4871f'),
+('5589216a3646e', '755', '5589216a48722'),
+('558922117fcef', 'echo', '5589221195248'),
+('558922117fcef', 'print', '558922119525a'),
+('558922117fcef', 'printf', '5589221195265'),
+('558922117fcef', 'cout', '5589221195270'),
+('55892211e44d5', 'int a', '55892211f1f97'),
+('55892211e44d5', '$a', '55892211f1fa7'),
+('55892211e44d5', 'long int a', '55892211f1fb4'),
+('55892211e44d5', 'int a$', '55892211f1fbd'),
+('558922894c453', 'cin>>a;', '558922895ea0a'),
+('558922894c453', 'cin<<a;', '558922895ea26'),
+('558922894c453', 'cout>>a;', '558922895ea34'),
+('558922894c453', 'cout<a;', '558922895ea41'),
+('558922899ccaa', 'cout', '55892289aa7cf'),
+('558922899ccaa', 'cin', '55892289aa7df'),
+('558922899ccaa', 'print', '55892289aa7eb'),
+('558922899ccaa', 'printf', '55892289aa7f5'),
+('558923538f48d', '255.0.0.0', '558923539a46c'),
+('558923538f48d', '255.255.255.0', '558923539a480'),
+('558923538f48d', '255.255.0.0', '558923539a48b'),
+('558923538f48d', 'none of these', '558923539a495'),
+('55892353f05c4', '192.168.1.100', '5589235405192'),
+('55892353f05c4', '172.168.16.2', '55892354051a3'),
+('55892353f05c4', '10.0.0.0.1', '55892354051b4'),
+('55892353f05c4', '11.11.11.11', '55892354051be'),
+('607336aa8c987', 'module.expose', '607336aa961a7'),
+('607336aa8c987', 'module', '607336aa961b1'),
+('607336aa8c987', 'module.exports', '607336aa961b5'),
+('607336aa8c987', 'all', '607336aa961b9'),
+('607336aacedd1', 'nodejs codeastro.js', '607336aadc686'),
+('607336aacedd1', 'node codeastro.js', '607336aadc68e'),
+('607336aacedd1', 'codeastro.js', '607336aadc691'),
+('607336aacedd1', 'none', '607336aadc694'),
+('607336ab244aa', 'npm --version', '607336ab31664'),
+('607336ab244aa', 'npm --ver', '607336ab31670'),
+('607336ab244aa', 'npm help', '607336ab31672'),
+('607336ab244aa', 'none', '607336ab31673');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `eid` text NOT NULL,
+  `qid` text NOT NULL,
+  `qns` text NOT NULL,
+  `choice` int(10) NOT NULL,
+  `sn` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`eid`, `qid`, `qns`, `choice`, `sn`) VALUES
+('558920ff906b8', '55892169bf6a7', 'what is command for changing user information??', 4, 1),
+('558920ff906b8', '5589216a3646e', 'what is permission for view only for other??', 4, 2),
+('558921841f1ec', '558922117fcef', 'what is command for print in php??', 4, 1),
+('558921841f1ec', '55892211e44d5', 'which is a variable of php??', 4, 2),
+('5589222f16b93', '558922894c453', 'what is correct statement in c++??', 4, 1),
+('5589222f16b93', '558922899ccaa', 'which command is use for print the output in c++?', 4, 2),
+('558922ec03021', '558923538f48d', 'what is correct mask for A class IP???', 4, 1),
+('558922ec03021', '55892353f05c4', 'which is not a private IP??', 4, 2),
+('6073360884420', '607336aa8c987', 'The node.js modules can be exposed using', 4, 1),
+('6073360884420', '607336aacedd1', 'Which statement executes the code of codeastro.js file?', 4, 2),
+('6073360884420', '607336ab244aa', 'How can we check the current version of NPM?', 4, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz`
+--
+
+CREATE TABLE `quiz` (
+  `eid` text NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `sahi` int(11) NOT NULL,
+  `wrong` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `time` bigint(20) NOT NULL,
+  `intro` text NOT NULL,
+  `tag` varchar(100) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `quiz`
+--
+
+INSERT INTO `quiz` (`eid`, `title`, `sahi`, `wrong`, `total`, `time`, `intro`, `tag`, `date`) VALUES
+('558920ff906b8', 'Linux : File Managment', 2, 1, 2, 5, '', 'linux', '2015-06-23 09:03:59'),
+('558921841f1ec', 'Php Coding', 2, 1, 2, 5, '', 'PHP', '2015-06-23 09:06:12'),
+('5589222f16b93', 'C++ Coding', 2, 1, 2, 5, '', 'c++', '2015-06-23 09:09:03'),
+('558922ec03021', 'Networking', 2, 1, 2, 5, '', 'networking', '2015-06-23 09:12:12'),
+('6073360884420', 'Nodejs Term', 2, 2, 3, 2, 'Basic test for nodejs terms', 'nodejs', '2021-04-11 17:46:48');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rank`
+--
+
+CREATE TABLE `rank` (
+  `email` varchar(50) NOT NULL,
+  `score` int(11) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `name` varchar(50) NOT NULL,
+  `gender` varchar(5) NOT NULL,
+  `college` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mob` bigint(20) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`name`, `gender`, `college`, `email`, `mob`, `password`) VALUES
+('Pravinpandey', 'M', 's.i.c.e.s', 'tvapp360@gmail.com', 7218449802, 'bfc5a92e2ced78c8d349ad414474547b');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`email`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
